a5f1cfe1b9e680371f67789a80dba1f0069d1e61
Kévin DANG <kevin.dang01@etu.univ-paris-diderot.fr>
2021-08-02 17:16:40 +0200

    config file

---
+++ backend/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    host: "localhost", //To complete
+    user: "", //To complete
+    password: "", //To complete
+    database: "microblog",
+};
+
+export default config;
---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+import mysql from "mysql2";
+import config from "./config.js";
+
+const db = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database,
+});
+
+export default db;
